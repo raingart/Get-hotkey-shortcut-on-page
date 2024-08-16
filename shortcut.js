6334cf9450f55af9624485cb437c7e1f59c6668e
raingart <13064767+raingart@users.noreply.github.com>
2024-08-16 13:25:41 +0300

    fix digit keycodes

--- shortcut.js
+++ shortcut.js
@@ -69,7 +69,7 @@ const shortcut = (() => {
       if (evt.altKey) pressedKeys.push('ALT');
       if (evt.metaKey) pressedKeys.push('META');
       if (!['CONTROL', 'SHIFT', 'ALT', 'META'].includes(evt.code)) {
-         const keyName = evt.code.replace(/^Key/, '').toUpperCase();
+         const keyName = evt.code.replace(/^(Key|Digit)/, '').toUpperCase();
          if (keyName !== evt.key && !evt.repeat) {
             pressedKeys.push(keyName);
          }
