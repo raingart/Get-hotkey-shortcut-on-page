18d576fb8ad4a5fe258e05eb668bdecf11707c0e
raingart <13064767+raingart@users.noreply.github.com>
2024-09-06 02:04:44 +0300

    Update shortcut.js

--- shortcut.js
+++ shortcut.js
@@ -55,7 +55,11 @@ const shortcut = (() => {
     * @returns {boolean}
     */
    const isInputDisabled = (target = required()) => {
-      return ['input', 'textarea', 'select'].includes(target.localName) || target.isContentEditable;
+      return (
+          ['TEXTAREA', 'SELECT'].includes(target.tagName)
+          || (target.tagName === 'INPUT' && ['email', 'text', 'password', 'search', 'url'].includes(target.getAttribute('type')))
+          || target.isContentEditable
+         );
    };
 
    /**
